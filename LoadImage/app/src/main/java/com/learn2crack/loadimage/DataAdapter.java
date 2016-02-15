12814a03655481e00f573acae41238fcb4e82025
Raj Amal W <raj.amalw@gmail.com>
2016-02-15 23:30:59 +0530

    Added project files

---
+++ LoadImage/app/src/main/java/com/learn2crack/loadimage/DataAdapter.java
@@ -0,0 +1,54 @@
+package com.learn2crack.loadimage;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
+    private ArrayList<AndroidVersion> android_versions;
+    private Context context;
+
+    public DataAdapter(Context context,ArrayList<AndroidVersion> android_versions) {
+        this.context = context;
+        this.android_versions = android_versions;
+
+    }
+
+    @Override
+    public DataAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.row_layout, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+
+        viewHolder.tv_android.setText(android_versions.get(i).getAndroid_version_name());
+        Picasso.with(context).load(android_versions.get(i).getAndroid_image_url()).resize(120, 60).into(viewHolder.img_android);
+    }
+
+    @Override
+    public int getItemCount() {
+        return android_versions.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tv_android;
+        ImageView img_android;
+        public ViewHolder(View view) {
+            super(view);
+
+            tv_android = (TextView)view.findViewById(R.id.tv_android);
+            img_android = (ImageView)view.findViewById(R.id.img_android);
+        }
+    }
+}
---
+++ LoadImage/app/src/main/java/com/learn2crack/loadimage/MainActivity.java
@@ -0,0 +1,68 @@
+package com.learn2crack.loadimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String android_version_names[] = {
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow"
+    };
+
+    private final String android_image_urls[] = {
+            "http://api.learn2crack.com/android/images/donut.png",
+            "http://api.learn2crack.com/android/images/eclair.png",
+            "http://api.learn2crack.com/android/images/froyo.png",
+            "http://api.learn2crack.com/android/images/ginger.png",
+            "http://api.learn2crack.com/android/images/honey.png",
+            "http://api.learn2crack.com/android/images/icecream.png",
+            "http://api.learn2crack.com/android/images/jellybean.png",
+            "http://api.learn2crack.com/android/images/kitkat.png",
+            "http://api.learn2crack.com/android/images/lollipop.png",
+            "http://api.learn2crack.com/android/images/marshmallow.png"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews(){
+         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.card_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<AndroidVersion> androidVersions = prepareData();
+        DataAdapter adapter = new DataAdapter(getApplicationContext(),androidVersions);
+        recyclerView.setAdapter(adapter);
+
+    }
+    private ArrayList<AndroidVersion> prepareData(){
+
+        ArrayList<AndroidVersion> android_version = new ArrayList<>();
+        for(int i=0;i<android_version_names.length;i++){
+            AndroidVersion androidVersion = new AndroidVersion();
+            androidVersion.setAndroid_version_name(android_version_names[i]);
+            androidVersion.setAndroid_image_url(android_image_urls[i]);
+            android_version.add(androidVersion);
+        }
+        return android_version;
+    }
+}
